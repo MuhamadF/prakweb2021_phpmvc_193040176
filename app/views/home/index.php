051736ddaba732e2d5f009010fda020c6a935d60
Muhamad Fawwazt <57528446+MuhamadF@users.noreply.github.com>
2021-10-01 17:16:53 +0700

    fix index home

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,26 +1,3 @@
-<!-- As a heading -->
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="<?= BASEURL; ?>/home">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="<?= BASEURL; ?>/about">About</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="<?= BASEURL; ?>/mahasiswa">Mahasiswa</a>
-        </li>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
 
 <div class="card">
   <div class="card-body"> 
